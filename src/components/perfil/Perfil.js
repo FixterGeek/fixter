b4fa68e585bcf2ae1baac6d8903ec1f7bd5a82ea
Héctor BlisS <hectorbliss@MacBook-Pro-de-Hector.local>
2019-02-12 00:25:13 -0600

    photo profile fixed

--- src/components/perfil/Perfil.js
+++ src/components/perfil/Perfil.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
-import './Perfil.css';
-import { PerfilDisplay } from './PerfilDisplay';
+import React, { Component } from "react";
+import "./Perfil.css";
+import { PerfilDisplay } from "./PerfilDisplay";
 
 class Perfil extends Component {
+	state = {
+		isLogged: false,
+		user: null
+	};
 
-    state = {
-        isLogged: false,
-        user: null
-    };
-
-    componentWillMount() {
-        let user = localStorage.getItem("user");
-        // user = JSON.parse(user);
-        if (user) {
-            // console.log("si")
-            this.setState({ isLogged: true, user })
-        } else {
-            this.setState({ isLogged: false })
-            this.props.history.push("/login");
-        }
-    }
-    componentDidMount() {
-        window.scroll(0, 0)
-    }
-    render() {
-        return (
-            <div>
-                <PerfilDisplay
-                    isLogged={this.props.isLogged}
-                    {...this.props.user}
-                />
-            </div>
-        );
-    }
+	componentWillMount() {
+		let user = JSON.parse(localStorage.getItem("user"));
+		//console.log(user);
+		// user = JSON.parse(user);
+		if (user) {
+			// console.log("si")
+			this.setState({ isLogged: true, user });
+		} else {
+			this.setState({ isLogged: false });
+			this.props.history.push("/login");
+		}
+	}
+	componentDidMount() {
+		window.scroll(0, 0);
+	}
+	render() {
+		let { user } = this.state;
+		return (
+			<div>
+				<PerfilDisplay isLogged={this.props.isLogged} {...user} />
+			</div>
+		);
+	}
 }
 
 export default Perfil;
