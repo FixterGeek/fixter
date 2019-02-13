630d257500b1867a9e6a7a43677737d59f6a99a1
DeividZavala <crusedmoss@gmail.com>
2019-02-13 16:48:17 -0600

    changing routes

--- src/services/aplications-service.js
+++ src/services/aplications-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const base_url = process.env.NODE_ENV === 'production' ? "https://fixter.camp" : "http://localhost:3000";
+const base_url = process.env.NODE_ENV === 'production' ? "https://fixtercamp.herokuapp.com" : "http://localhost:3000";
 
 export  const createAplication = (obj) => {
 	return axios.post(`${base_url}/aplications/`, obj)
