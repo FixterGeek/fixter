62e20cad6e5da0672bb4c7d88b0043e685925e5b
DeividZavala <crusedmoss@gmail.com>
2019-02-19 10:30:19 -0600

    curso pachuca

--- src/components/bootcamp/Bootcamp.js
+++ src/components/bootcamp/Bootcamp.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Bootcamp.css';
 import bootcamp from '../../assets/bootc.png';
 import temario_edition from '../../assets/temarios/Reacjs hooks + firebase.pdf';
+import pachuca from  '../../assets/52265924_1560764037401880_6264642566415712256_n.png'
 import {CardCampDisplay} from '../CardCamp/CardCampDisplay';
 
 
@@ -35,6 +36,21 @@ class Bootcamp extends Component {
                         precio="12,000.00"
 
                     />
+
+				   <CardCampDisplay
+					   nombre="React Hooks + Firebase Pachuca"
+					   descript="Aprenderás todo lo que necesitas sobre la programación de una aplicación web con JavaScript y ReactJs junto con su más reciente lanzamiento... Hooks. Además, aprenderás Firebase, un backend as a service de Google, usando principalmente su base de datos no relacional, teniendo con esto una aplicación fullstack completa sin tener que crear un backend en ningún otro lenguaje."
+					   imagen={pachuca}
+					   fecha="25 de Marzo"
+					   temario={temario_edition}
+					   place="Peanut cowork"
+					   address="Av, Blvrd Nuevo Hidalgo 228, Puerta de Hierro, Pachuca de Soto, Hgo"
+					   dia="Lun, Mar y Mie"
+					   horario="5:00 pm a 8:00 pm"
+					   precio="15,000.00"
+
+				   />
+
                 </div>
 
             </div>
