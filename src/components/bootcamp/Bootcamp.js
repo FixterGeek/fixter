1b84359bca386a08caf8df5006a5e5ada6741b03
DeividZavala <crusedmoss@gmail.com>
2019-03-08 20:15:13 -0600

    adding courses to bootcamps section

--- src/components/bootcamp/Bootcamp.js
+++ src/components/bootcamp/Bootcamp.js
@@ -5,6 +5,10 @@ import temario_edition from '../../assets/temarios/Reacjs hooks + firebase.pdf';
 import pachuca from  '../../assets/52265924_1560764037401880_6264642566415712256_n.png'
 import {CardCampDisplay} from '../CardCamp/CardCampDisplay';
 import temario_pachuca from '../../assets/temarios/Reacjs-firebase-Pachuca.pdf'
+import image_vue from '../../assets/53676946_2404645959559794_8377063136498286592_o.png'
+import vue_temario from "../../assets/temarios/Vue + Typescript.pdf"
+import image_python from '../../assets/53643428_2404605299563860_1164970498484862976_o.jpg'
+import temario_python from '../../assets/temarios/Python + Django Rest Framework.pdf'
 
 
 class Bootcamp extends Component {
@@ -24,18 +28,33 @@ class Bootcamp extends Component {
 
 				<a href="fire-hooks" >
 					<CardCampDisplay
-                        nombre="React Hooks + Firebase"
-                        descript="Aprenderás todo lo que necesitas sobre la programación de una aplicación web con JavaScript y ReactJs junto con su más reciente lanzamiento... Hooks. Además, aprenderás Firebase, un backend as a service de Google, usando principalmente su base de datos no relacional, teniendo con esto una aplicación fullstack completa sin tener que crear un backend en ningún otro lenguaje."
-                        imagen="https://i.imgur.com/8hbDoO0.jpg"
-                        fecha="4 de Marzo"
-						sabatino={true}
-						temario={temario_edition}
+                        nombre="Vue + TypeScript"
+                        descript="Como desarrolladores web sabemos lo importante que es seleccionar la herramienta correcta y de menor fricción para contruir aplicaciones de forma rápida, sencilla y poderosa. VUE se ha convertido en los últimos años en una de las opciones más populares gracias a que cumple estas caracteristicas haciendo crecer su comunidad y que cada vez más startups y desarrolladores decidan usarlo, crea aplicaciones web de la una forma rápida y moderna."
+                        imagen={image_vue}
+                        fecha="1 de Abril"
+						sabatino={false}
+						temario={vue_temario}
 						place="Cubic Idea"
 						address="Jalapa 100, Roma Nte., CDMX"
                         dia="Part time"
                         precio="12,000.00"
 
                     />
+
+					<CardCampDisplay
+						nombre="Python + Django Rest Framework"
+						descript="Según githut.info (sí, githut) Python es uno de los tres lenguajes más importantes en la actualidad. En esta ocación Python será nuestra herramienta principal con la que vamos a construir API's REST, uno de los estándares más modernos para el desarrollo de micro servicios web. Inscríbete y conviertete en un desarrollador backend rockstar."
+						imagen={image_python}
+						fecha="2 de Abril"
+						sabatino={true}
+						temario={temario_python}
+						place="Cubic Idea"
+						address="Jalapa 100, Roma Nte., CDMX"
+						dia="Part time"
+						precio="12,000.00"
+
+					/>
+
 							</a>
 							<a href="fire-pachuca" >
 				   <CardCampDisplay
